17a782941c307c7ab2a80f77afd6d9e3b4336b25
Isaac <isaac.ruiz02@utrgv.edu>
2017-07-18 16:09:14 -0500

    Set up character arrays

--- Polyomino/Printer/printer.cpp
+++ Polyomino/Printer/printer.cpp
@@ -2,8 +2,9 @@
 
 void print(string s)
 {
-	string c[] = {"┌","┐","└", "┘","─","│","┼"};
-	
+	string corner[] = {"┌─","─┐","└─", "─┘", "┌","┐","└", "┘"};
+	string bar[] = {"──","│"};                  
+	string intersection[] = {"┼─","─┼", "─┤", "├─", "┴─", "─┴", "┬─", "─┬"};
 	string array[20][20];	
 	for (int i = 0; i < s.length(); i++)
 	{
@@ -12,22 +13,6 @@ void print(string s)
 			array [i][j] = " ";
 		}
 	}
-	array[0][2] = c[0];
-	for(int i = 3; i < 6; i++)
-		array [0][i] = c[4];
-	array[0][6] = c[1];	
-	array[1][6] = c[6];
-	array [2][6] = c[3];
-	for(int i = 3; i < 6; i++)
-		array[2][i] = c[4];
-
-	array[2][2] = c[2];
-	array[1][2] = c[5];
-	
-	for(int i = 0; i < s.length(); i++){
-		for(int j = 0; j < s.length(); j++)
-			cout << array[i][j];
-		cout << endl;
-	}
 
 }
+
